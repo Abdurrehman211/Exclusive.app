56030de7f17b49654bd18072872f0cdd4ccec013
Huzaifa Niazi <niazih726@gmail.com>
2025-03-08 10:36:55 -0800

    add form functionality to Contact component with styling adjustments

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,8 +1,25 @@
-import React from "react"
+import React, { useState } from "react"
 import "./contact.css";
 
 function Contact ()
 {
+
+    
+        const [name, setname] = useState("");
+        const [mail, setmail] = useState("");
+        const [message, setmessage] = useState("");
+        const [phone, setphone] = useState("");
+
+        //handle form submission
+        const handle=(e)=>{
+          e.preventDefault();
+          alert ("message send");
+            setname("");
+            setmail("");
+            setmessage("");
+            setphone("");
+        };
+    
     return (
         
         <div>
@@ -15,6 +32,39 @@ function Contact ()
                 <p id="p5"> Fill out your form and we will contact <br/>you  within 24 hours</p>
                 <p id="p6">Email: customer@exclusive.com</p>
                 <p id="p7">Email: support@exclusive.com</p>
+            </div>
+            <div className="class2">
+                <form onSubmit={handle}>
+                
+                <input
+                type="text"
+                placeholder="Your name"
+                value={name}
+                onChange={(e)=>setname(e.target.value)}
+                />
+               
+                <input
+                type="email"
+                placeholder="Your email"
+                value={mail}
+                onChange={(e)=>setmail(e.target.value)}
+
+               />
+               
+               <input
+                type="text"
+                placeholder="Your phone"
+                value={phone}
+                onChange={(e)=>setphone(e.target.value)}
+               />
+              
+               <textarea
+                value={message}
+                placeholder="Your message"
+                onChange={(e) => setmessage(e.target.value)}
+               />
+</form>
+<button id="b1" type="submit">Send Message</button>
             </div>
         </div>
     )
