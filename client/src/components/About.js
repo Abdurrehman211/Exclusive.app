d8b6bbe899bec1452238e93d63af05a761c1dbbc
Huzaifa Niazi <niazih726@gmail.com>
2025-03-16 03:32:13 -0700

    add About component and associated styles

---
+++ client/src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './about.css';
+function About ()
+{
+    <>
+    <div className="class1">
+        <h> Our Story</h>
+        <p> Launche in 2015 ,Exculisve in fjsho;andaand <br/> kdsagjhibnglsngfd jngogo gng r/nfng rengrgn <br/>iofh hudnff hfueifhh nfelf bfekfb fkebfeb</p>
+        <p> flnon fjdnfo ekjfnneo kefb <br/> efnlfndlfflnf <br/> fnknldnfldf jdala </p>
+
+    </div>
+    </>
+}
+ export default About;
