ba5ce69a48f97dc840f79b1e8883900370133bdb
Huzaifa Niazi <niazih726@gmail.com>
2025-03-27 04:25:10 -0700

    enhance About component layout with new statistics cards and improved content

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -4,7 +4,8 @@ function About ()
 {
     return (
     <>
-    <div className='container'>
+    <div className='container'> 
+        
 <div className="col-lg-6 col-md-12 col-sm-12"> 
     <div className='our-story'> 
        <h1> Our Story</h1>
@@ -15,9 +16,31 @@ function About ()
 <div className="col-lg-6 col-md-12 col-sm-12">
     <div className='picture'>
         sdjdnsjdsndjsdbkjb
-
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus illum tempora optio, tenetur ducimus sunt eum culpa sint nemo, ex dolorem repellat eos adipisci officia eaque molestiae rem totam voluptas.</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus atque nobis, sint dolor earum non sunt delectus quidem, necessitatibus commodi quae veritatis voluptatem! Itaque illum illo, debitis deleniti error natus.</p>
     </div>
 </div>
+<div className='cardes col-lg-12 col-md-12 col-sm-12 mt-5'>
+    <div className='card1'> 
+        <h2> 10.5k</h2>
+        <p> Saller active our site</p>
+    </div>
+    <div className='card2'> 
+        <h2> 33k</h2>
+        <p> Monthly product sale</p>
+    </div>
+    <div className='card3 '> 
+        <h2> 44.5k</h2>
+        <p> Custome active on our site</p>
+    </div>
+    <div className='card4'> 
+        <h2> 10k</h2>
+        <p> Average gross sale in our sute</p>
+    </div>
+    </div>
+
+
+
 
     </div>
     </>
