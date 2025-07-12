9aae9e8b0c3e166cb2bcac63bb846785ec54515c
abdur rehman abid <156325681+Abdurrehman211@users.noreply.github.com>
2025-07-12 23:31:08 +0500

    chanes

--- Server/index.js
+++ Server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const nodemailer = require("nodemailer");
 const cors = require("cors");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const { OAuth2Client } = require("google-auth-library");
